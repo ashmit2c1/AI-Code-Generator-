126ff40e51beb629743cf5d541068a098e1baade
ashmit2c1 <codeatashmit@gmail.com>
2025-07-04 14:52:35 +0530

    Working on Agent and Command Line arguments

--- cmd/maker/main.go
+++ cmd/maker/main.go
@@ -3,22 +3,29 @@ package main
 import (
 	"ai_code_gen/internals/agents"
 	"context"
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	key := "{OPENAI_API_SECRET_KEY}"
+	openAPIKey := flag.String("openai-key", "", "OpenAI API Key")
+	outputDir := flag.String("output-dir", "./output", "Output directory for generated files")
+	basePackage := flag.String("base-package", "github.com/user/app", "Base package for generated files")
+	workerCount := flag.Int("worker-count", 4, "Number of workers to use for file generation")
+
+	flag.Parse()
+	if *openAPIKey == "" {
+		*openAPIKey = "{OPENAI_API_SECRET_KEY}"
+		if *openAPIKey == "" {
+			fmt.Println("Please provide with OpenAI API Key")
+			os.Exit(1)
+		}
+	}
 
 	cntxt := context.Background()
 
-	prompt := "Write a simple todo program"
-
-	openAIClient := agents.NewOpenAPI(cntxt, key, nil)
-
-	res, err := openAIClient.Query("", prompt)
-
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%+v", res)
+	openAIClient := agents.NewOpenAPI(cntxt, *openAPIKey, nil)
+	agents.NewAgent(cntxt, openAIClient, *outputDir, *basePackage, *workerCount)
+	fmt.Println("READY")
 }
--- internals/agents/agent.go
+++ internals/agents/agent.go
@@ -1,1 +1,40 @@
 package agents
+
+import (
+	"context"
+	"sync"
+)
+
+// When we get a response from OpenAPI we want to
+type FileTask struct {
+	Path    string
+	Content string
+}
+
+type Agent struct {
+	openAI           *OpenAPI
+	outputDir        string
+	basePackage      string
+	taskQueue        chan FileTask
+	wg               sync.WaitGroup
+	workerCount      int
+	cntxt            context.Context
+	cancel           context.CancelFunc
+	fileWrittenMutex sync.Mutex
+	filesWritten     map[string]bool
+}
+
+func NewAgent(cntxt context.Context, openAPI *OpenAPI, outputDir string, basePackage string, workerCount int) *Agent {
+	// context.Context, context.CancelFunc
+	cntxt, cancel := context.WithCancel(cntxt)
+	return &Agent{
+		openAI:       openAPI,
+		outputDir:    outputDir,
+		basePackage:  basePackage,
+		taskQueue:    make(chan FileTask, 100),
+		workerCount:  workerCount,
+		cntxt:        cntxt,
+		cancel:       cancel,
+		filesWritten: make(map[string]bool),
+	}
+}
